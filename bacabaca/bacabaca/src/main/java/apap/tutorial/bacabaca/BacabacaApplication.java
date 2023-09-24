9088a8d8868475e0f12e155dd0b5e788dba47cfa
maritzarahayu <maritzarahayui@gmail.com>
2023-09-24 14:57:12 +0700

    generate 50 penerbit dan 20 buku

--- bacabaca/bacabaca/src/main/java/apap/tutorial/bacabaca/BacabacaApplication.java
+++ bacabaca/bacabaca/src/main/java/apap/tutorial/bacabaca/BacabacaApplication.java
@@ -34,27 +34,56 @@ public class BacabacaApplication {
 			var faker = new Faker(new Locale("in-ID"));
 
 			// Membuat fake data memanfaatkan Java Faker
-			var bukuDTO = new CreateBukuRequestDTO();
-			var fakeBook = faker.book();
-			var fakeDate = faker.date();
+//			var bukuDTO = new CreateBukuRequestDTO();
+//			var fakeBook = faker.book();
+//			var fakeDate = faker.date();
 
-			bukuDTO.setHarga(new BigDecimal(Math.random()*1000000));
-			bukuDTO.setJudul(fakeBook.title());
-			bukuDTO.setTahunTerbit(String.valueOf(fakeDate.past(36500, TimeUnit.DAYS).getYear()+1900));
+//			bukuDTO.setHarga(new BigDecimal(Math.random()*1000000));
+//			bukuDTO.setJudul(fakeBook.title());
+//			bukuDTO.setTahunTerbit(String.valueOf(fakeDate.past(36500, TimeUnit.DAYS).getYear()+1900));
 
-			var penerbitDTO = new CreatePenerbitRequestDTO();
-			penerbitDTO.setNamaPenerbit(fakeBook.publisher());
-			penerbitDTO.setEmail(faker.internet().emailAddress());
-			penerbitDTO.setAlamat(faker.address().fullAddress());
+//			var penerbitDTO = new CreatePenerbitRequestDTO();
+//			penerbitDTO.setNamaPenerbit(fakeBook.publisher());
+//			penerbitDTO.setEmail(faker.internet().emailAddress());
+//			penerbitDTO.setAlamat(faker.address().fullAddress());
 
 			// Mapping penerbitDTO ke buku lalu save penerbit ke database
-			var penerbit = penerbitMapper.createPenerbitRequestDTOToPenerbit(penerbitDTO);
-			penerbit = penerbitService.createPenerbit(penerbit);
+//			var penerbit = penerbitMapper.createPenerbitRequestDTOToPenerbit(penerbitDTO);
+//			penerbit = penerbitService.createPenerbit(penerbit);
 
 			// Mapping bukuDTO ke buku lalu save buku ke database
-			var buku = bukuMapper.createBukuRequestDTOToBuku(bukuDTO);
-			buku.setPenerbit(penerbit);
-			bukuService.saveBuku(buku);
+//			var buku = bukuMapper.createBukuRequestDTOToBuku(bukuDTO);
+//			buku.setPenerbit(penerbit);
+//			bukuService.saveBuku(buku);
+
+
+			// Generate 50 publishers (Penerbit)
+			for (int i = 1; i <= 50; i++) {
+				var penerbitDTO = new CreatePenerbitRequestDTO();
+				penerbitDTO.setNamaPenerbit(faker.book().publisher());
+				penerbitDTO.setEmail(faker.internet().emailAddress());
+				penerbitDTO.setAlamat(faker.address().fullAddress());
+
+				// Mapping penerbitDTO to Penerbit and save to the database
+				var penerbit = penerbitMapper.createPenerbitRequestDTOToPenerbit(penerbitDTO);
+				penerbit = penerbitService.createPenerbit(penerbit);
+
+				// Generate 20 books (Buku) and associate them with the publisher
+				for (int j = 1; j <= 20; j++) {
+					var bukuDTO = new CreateBukuRequestDTO();
+					var fakeBook = faker.book();
+					var fakeDate = faker.date();
+
+					bukuDTO.setHarga(new BigDecimal(Math.random() * 1000000));
+					bukuDTO.setJudul(fakeBook.title());
+					bukuDTO.setTahunTerbit(String.valueOf(fakeDate.past(36500, TimeUnit.DAYS).getYear() + 1900));
+
+					// Mapping bukuDTO to Buku and save to the database
+					var buku = bukuMapper.createBukuRequestDTOToBuku(bukuDTO);
+					buku.setPenerbit(penerbit);
+					bukuService.saveBuku(buku);
+				}
+			}
 		};
 	}
 
